577105f0a196cdc3cfe8c057f29e66af3f8f1e3b
Holly Schilling <holly.a.schilling@outlook.com>
2017-03-07 14:27:57 -0600

    Added ItemCollectionReusableView

---
+++ EssentialElements/EssentialElements/ItemLists/Views/ItemCollectionReusableView.swift
@@ -0,0 +1,44 @@
+//
+//  ItemCollectionReusableView.swift
+//  EssentialElements
+//
+//  Created by Holly Schilling on 3/7/17.
+//  Copyright © 2017 Better Practice Solutions. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Foundation
+import UIKit
+
+open class ItemCollectionReusableView<ItemViewType: UIView>: UICollectionReusableView {
+        
+    open var itemView: ItemViewType = ItemViewType(frame: CGRect.zero)
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addFullSized(subview: itemView)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        addFullSized(subview: itemView)
+    }
+}
